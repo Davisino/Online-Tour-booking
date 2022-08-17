f725161dafa918da3ab38354d15dace11c598b8a
luisdavid <david.laleala@gmail.com>
2022-08-17 19:29:59 +0100

    update

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -14,9 +14,11 @@ const handleValidationErrorDB = (err) => {
 
   return new AppError(message, 400);
 };
-const handleJWTError = (err) =>
+const handleJWTError = () =>
   new AppError('Invalid token. Please login again', 401);
 
+const handleJWTExpiredError = () =>
+  new AppError('Your token has expired. Please log in again', 401);
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -60,6 +62,8 @@ module.exports = (err, req, res, next) => {
     if (error.name === 'ValidationError')
       error = handleValidationErrorDB(error);
     if (error.name === 'JsonWebTokenError') error = handleJWTError(error);
+    if (error.name === 'TokenExpiredError')
+      error = handleJWTExpiredError(error);
     sendErrorProd(error, res);
   }
 };
